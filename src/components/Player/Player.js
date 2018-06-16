3b9f875d20c0d917029811ff25c8f2fcd40f8ede
Tomasz Paterka <tomekpaterka@gmail.com>
2018-06-16 15:50:15 +0200

    add Player component and tests

---
+++ src/components/Player/Player.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Player.css';
+
+const Player = (props) => (
+	<li className="Player">
+		<span className="Player_name">{props.name}</span>
+		<span className="Player_score">{props.score}</span>
+		<span className="Player_button" onClick={() => props.onPlayerScoreChange(1)}>+</span>
+		<span className="Player_button" onClick={() => props.onPlayerScoreChange(-1)}>-</span>
+	</li>
+);
+
+export default Player;
---
+++ src/components/Player/Player.test.js
@@ -0,0 +1,46 @@
+import Player from './Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+	shallow(<Player />);
+});
+
+it('renders correct name', () => {
+	const playerNamePassed = 'Ania';
+	const playerComponent = shallow(<Player name={playerNamePassed}/>);
+	
+	const playerNameRendered = playerComponent.find('.Player_name').text();
+	
+	expect(playerNameRendered).toEqual(playerNamePassed);
+});
+
+it('renders correct score', () => {
+	const playerScorePassed = 5;
+	const playerComponent = shallow(<Player score={playerScorePassed}/>);
+
+	const playerScoreRendered = parseInt(playerComponent.find('.Player_score').text());
+	//const playerScoreRendered = Number(playerComponent.find('.Player_score').text());
+
+	expect(playerScoreRendered).toEqual(playerScorePassed);
+});
+
+it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
+	const mockedOnPlayerScoreChange = jest.fn();
+	const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+	const plusButton = playerComponent.find('.Player_button').at(0);
+	plusButton.simulate('click');
+	
+	expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+});
+
+it('should call onPlayerScoreChange with -1 when minus button is clicked', () => {
+	const mockedOnPlayerScoreChange = jest.fn();
+	const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+	const minusButton = playerComponent.find('.Player_button').at(1);
+	minusButton.simulate('click');
+
+	expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+});
